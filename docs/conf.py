6d91d39f4062d96f9b82b3101bddd48c748eefaf
Nathan Henrie <nate@n8henrie.com>
2016-01-11 16:16:15 -0700

    Use RTD theme if building locally

--- docs/conf.py
+++ docs/conf.py
@@ -19,6 +19,8 @@ import glob
 import shutil
 from recommonmark.parser import CommonMarkParser
 
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
 # If extensions (or modules to document with autodoc) are in another
 # directory, add these directories to sys.path here. If the directory is
 # relative to the documentation root, use os.path.abspath to make it
@@ -117,7 +119,11 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
