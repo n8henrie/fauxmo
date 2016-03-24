a5f9c4df87501d4f954d46e565d15e7f73f838a0
Nathan Henrie <nate@n8henrie.com>
2016-03-24 09:59:45 -0600

    Add additional HTTP verbs / options in RestApiHandler
    Expanded the potential of RestApiHandler by modifying it to accept other
    HTTP verbs, POST data, and allowing it to have different data sent for
    `on()` and `off()` methods. This will mean most users now have a bit of
    redundancy in their configs but should greatly expand the number of
    devices that `RestAPIHandler` can interact with.
    
    Breaking changes
    ===
    
    - RestApiHandler's `json` kwarg is now either `on_json` or `off_json`,
      to allow different JSON bodies to be sent for `on()` or `off()`
    
    Additions
    ---
    
    - Now also accepts `on_data`, `off_data` for POST bodies
    - Will now work with other HTTP verbs like `put`
    - Now supports HTTP Basic and Digest authentication
    - Reduced redundancy in `on()` and `off()` methods with
      `functools.partialmethod`

--- fauxmo/handlers/rest.py
+++ fauxmo/handlers/rest.py
@@ -1,34 +1,66 @@
 import requests
+from requests.auth import HTTPBasicAuth, HTTPDigestAuth
+from functools import partialmethod
 
 
 class RestApiHandler:
     """Rest API handler class.
 
     The Fauxmo class expects handlers to be instances of objects that have on()
-    and off() methods that return True on success and False otherwise.  This
-    example class takes two full URLs that should be requested when an on and
-    off command are invoked respectively. It ignores any return data.
+    and off() methods that return True on success and False otherwise. This
+    class takes a mix of url, method, header, body, and auth data and makes
+    REST calls to a device.
     """
 
-    def __init__(self, on_cmd, off_cmd, method=None, json=None, headers=None):
+    def __init__(self, on_cmd, off_cmd, method="GET", on_data=None,
+                 off_data=None, on_json=None, off_json=None, headers=None,
+                 auth_type=None, user=None, password=None):
+        """Initialize a RestApiHandler.
+
+        Args:
+            on_cmd (str): URL to be called when turning device on
+            off_cmd (str): URL to be called when turning device off
+
+        Kwargs:
+           method (str): HTTP method to be used for both `on()` and `off()`
+           on_data (dict): HTTP body to turn device on
+           off_data (dict): HTTP body to turn device off
+           on_json (dict): JSON body to turn device on
+           off_json (dict): JSON body to turn device off
+           headers (dict): Additional headers for both `on()` and `off()`
+           auth_type (str): Either `basic` or `digest` if needed
+           user (str): Username for `auth`
+           password (str): Password for `auth`
+       """
+
+        self.method = method
+        self.headers = headers
+        self.auth = None
+
         self.on_cmd = on_cmd
         self.off_cmd = off_cmd
-        self.method = method
-        self.json = json
-        self.headers = headers
 
-    def on(self):
-        if self.method == "POST":
-            r = requests.post(self.on_cmd, json=self.json,
-                              headers=self.headers)
-        else:
-            r = requests.get(self.on_cmd, headers=self.headers)
+        self.on_data = on_data
+        self.off_data = off_data
+
+        self.on_json = on_json
+        self.off_json = off_json
+
+        if auth_type:
+            if auth_type.lower() == "basic":
+                self.auth = HTTPBasicAuth(user, password)
+
+            elif auth_type.lower() == "digest":
+                self.auth = HTTPDigestAuth(user, password)
+
+    def set_state(self, cmd, data, json):
+        """Call HTTP method, for use by `functools.partialmethod`."""
+
+        r = requests.request(self.method, getattr(self, cmd),
+                             data=getattr(self, data),
+                             json=getattr(self, json), headers=self.headers,
+                             auth=self.auth)
         return r.status_code in [200, 201]
 
-    def off(self):
-        if self.method == "POST":
-            r = requests.post(self.off_cmd, json=self.json,
-                              headers=self.headers)
-        else:
-            r = requests.get(self.off_cmd, headers=self.headers)
-        return r.status_code in [200, 201]
+    on = partialmethod(set_state, 'on_cmd', 'on_data', 'on_json')
+    off = partialmethod(set_state, 'off_cmd', 'off_data', 'off_json')
