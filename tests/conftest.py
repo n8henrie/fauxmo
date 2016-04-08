5a150a97d66aac4b328ce94c1d9b1a217ed67230
Nathan Henrie <nate@n8henrie.com>
2016-04-08 09:25:41 -0600

    Update tests for asyncio
    For testing, use `multiprocessing` to create a Fauxmo server in a
    thread with a pytest `yield_fixture`, which we can then use to test our
    UDP and TCP servers using `socket` and `requests`, respectively. Of
    note, the `yield_fixture` is only creating the server once for the
    entire testing session (`scope=("session")`), so while a brief
    (currently 1 second) delay is required to allow Fauxmo time to set up
    the server, this is only required once for all tests. Also had to
    include a 1 second timeout for the UDP server to respond for some reason
    (in `test_udp_search`).

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,29 +1,16 @@
 import pytest
 from fauxmo import fauxmo
-import threading
+from multiprocessing import Process
 from time import sleep
-import http.server
-import json
-import socketserver
 
-@pytest.fixture(scope="session")
+
+@pytest.yield_fixture(scope="session")
 def fauxmo_server():
-    server = threading.Thread(target=fauxmo.main,
-                             kwargs={'config_path': "tests/test_config.json"},
-                             daemon=True)
+    server = Process(target=fauxmo.main,
+                     kwargs={'config_path': "tests/test_config.json"},
+                     daemon=True)
     server.start()
     sleep(1)
-    return
-
-
-@pytest.fixture(scope="session")
-def fake_switch_one():
-    with open('tests/test_config.json') as f:
-        config = json.load(f)
-    port = 12345
-
-    Handler = http.server.SimpleHTTPRequestHandler
-    httpd = socketserver.TCPServer(("localhost", 2345), Handler)
-    httpd_thread = threading.Thread(target=httpd.serve_forever, daemon=True)
-    httpd_thread.start()
-    return
+    yield
+    server.terminate()
+    server.join()
--- tests/test_fauxmo.py
+++ tests/test_fauxmo.py
@@ -1,13 +1,28 @@
 """test_fauxmo.py
+
 Tests for `fauxmo` module.
 """
 
-import pytest
-import fauxmo.fauxmo
 import requests
 import xml.etree.ElementTree as etree
+import pytest
+import socket
+
+
+def test_udp_search(fauxmo_server):
+    """Test device search request to UPnP / SSDP server"""
+
+    msg = b'urn:Belkin:device:**'
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(1)
+    sock.sendto(msg, ('localhost', 1900))
+    data = sock.recv(4096)
+
+    assert b'LOCATION: http://localhost:12345/setup.xml' in data
+
 
 def test_setup(fauxmo_server):
+    """Test TCP server's `/setup.xml` endpoint"""
 
     r = requests.get('http://localhost:12345/setup.xml')
     assert r.status_code == 200
@@ -16,10 +31,14 @@ def test_setup(fauxmo_server):
     assert root.find(".//friendlyName").text == 'fake switch one'
 
 
-def test_turnon(fauxmo_server, fake_switch_one):
-    headers = {'SOAPACTION':
-               '"urn:Belkin:service:basicevent:1#SetBinaryState"'}
-    payload = '<BinaryState>1</BinaryState>'
+def test_turnon(fauxmo_server):
+    """Test TCP server's "on" action for RESTAPIHandler"""
 
-    r = requests.get('http://localhost:12345', headers=headers, data=payload)
-    # assert r.status_code == 200
+    data = '<BinaryState>1</BinaryState>'
+
+    # requests.exceptions.Timeout suggests it was able to issue the request to
+    # Fauxmo without exception, but there aren't any devices running at the
+    # configured `on_cmd` address (probably should be true)
+    with pytest.raises(requests.exceptions.Timeout):
+        requests.post('http://127.0.0.1:12345/upnp/control/basicevent1',
+                      data=data, timeout=1)
