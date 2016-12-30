627e16a4bae8e9d8c175453f730d0bc411010154
Nathan Henrie <nate@n8henrie.com>
2016-12-30 12:37:30 -0700

    Reduce default logging verbosity

--- src/fauxmo/cli.py
+++ src/fauxmo/cli.py
@@ -17,11 +17,12 @@ def cli():
     parser = argparse.ArgumentParser(description="Emulate Belkin Wemo devices "
                                      "for use with Amaazon Echo")
     parser.add_argument("-v", "--verbose", help="increase verbosity (may "
-                        "repeat up to -vv)", action="count", default=1)
+                        "repeat up to -vvv)", action="count", default=0)
     parser.add_argument("-c", "--config", help="specify alternate config file")
     args = parser.parse_args(arguments)
 
-    # args.verbose defaults to 1 -> 30 == logging.WARNING
+    # args.verbose defaults to 0
+    # 40 - 10 * 0 = 40 == logging.ERROR
     verbosity = max(40 - 10 * args.verbose, 10)
     logger.setLevel(verbosity)
 
