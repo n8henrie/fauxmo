3f5a5dbcad7122237b729164068cde819b5c995c
Nathan Henrie <nate@n8henrie.com>
2017-03-21 16:26:16 -0600

    Update for fauxmo v0.4.0 and plugins
    Also add a test server based on httpbin to ensure fauxmo plugins' `on`
    is getting called properly

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,32 @@
-import pytest
-from fauxmo import fauxmo
+"""conftest.py :: Setup fixtures for pytest."""
+
 from multiprocessing import Process
 from time import sleep
 
+import httpbin
+import pytest
+from fauxmo import fauxmo
 
-@pytest.yield_fixture(scope="session")
+
+@pytest.fixture(scope="session")
 def fauxmo_server():
+    config_path = "tests/test_config.json"
     server = Process(target=fauxmo.main,
-                     kwargs={'config_path': "tests/test_config.json"},
+                     kwargs={'config_path': config_path},
                      daemon=True)
+
+    fauxmo_device = Process(target=httpbin.core.app.run,
+                            kwargs={"host": "127.0.0.1", "port": 8000},
+                            daemon=True)
+
     server.start()
+    fauxmo_device.start()
     sleep(1)
+
     yield
+
+    fauxmo_device.terminate()
+    fauxmo_device.join()
+
     server.terminate()
     server.join()
--- tests/test_fauxmo.py
+++ tests/test_fauxmo.py
@@ -1,12 +1,12 @@
-"""test_fauxmo.py
+"""test_fauxmo.py :: Tests for `fauxmo` package."""
 
-Tests for `fauxmo` module.
-"""
+import json
+import socket
+import xml.etree.ElementTree as etree
+
+from fauxmo.plugins.simplehttphandler import SimpleHTTPHandler
 
 import requests
-import xml.etree.ElementTree as etree
-import pytest
-import socket
 
 
 def test_udp_search(fauxmo_server):
@@ -18,13 +18,14 @@ def test_udp_search(fauxmo_server):
     sock.sendto(msg, ('localhost', 1900))
     data = sock.recv(4096)
 
-    assert b'LOCATION: http://localhost:12345/setup.xml' in data
+    assert b'LOCATION: http://' in data
+    assert b'/setup.xml' in data
 
 
 def test_setup(fauxmo_server):
     """Test TCP server's `/setup.xml` endpoint"""
 
-    r = requests.get('http://localhost:12345/setup.xml')
+    r = requests.get('http://127.0.0.1:12345/setup.xml')
     assert r.status_code == 200
 
     root = etree.fromstring(r.text)
@@ -32,13 +33,23 @@ def test_setup(fauxmo_server):
 
 
 def test_turnon(fauxmo_server):
-    """Test TCP server's "on" action for RESTAPIHandler"""
+    """Test TCP server's "on" action for SimpleHTTPHandler"""
 
     data = '<BinaryState>1</BinaryState>'
 
     # requests.exceptions.Timeout suggests it was able to issue the request to
     # Fauxmo without exception, but there aren't any devices running at the
     # configured `on_cmd` address (probably should be true)
-    with pytest.raises(requests.exceptions.Timeout):
-        requests.post('http://127.0.0.1:12345/upnp/control/basicevent1',
-                      data=data, timeout=1)
+    resp = requests.post('http://127.0.0.1:12345/upnp/control/basicevent1',
+                         data=data)
+    assert resp.status_code == 200
+
+
+def test_simplehttphandler(fauxmo_server):
+    with open("tests/test_config.json") as f:
+        config = json.load(f)
+
+    for device in config["PLUGINS"]["SimpleHTTPHandler"]["DEVICES"]:
+        print(device)
+        assert SimpleHTTPHandler(**device).on() is True
+        assert SimpleHTTPHandler(**device).off() is True
