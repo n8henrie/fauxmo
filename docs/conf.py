27248135739f71f060a63a646bce13d8c19caa71
Nathan Henrie <nate@n8henrie.com>
2017-04-07 19:18:57 -0600

    Try napolean docstring parsing

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,8 @@ import fauxmo  # noqa
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
         'sphinx.ext.autodoc',
-        'sphinx.ext.viewcode'
+        'sphinx.ext.viewcode',
+        'sphinx.ext.napoleon',
         ]
 
 # Add any paths that contain templates here, relative to this directory.
