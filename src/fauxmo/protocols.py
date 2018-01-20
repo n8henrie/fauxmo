c74b374447704545e1970806a3e6148757abc595
Stavros Korokithakis <hi@stavros.io>
2018-01-20 17:30:00 +0200

    Remove all f-strings

--- src/fauxmo/protocols.py
+++ src/fauxmo/protocols.py
@@ -36,7 +36,7 @@ class Fauxmo(asyncio.Protocol):
             transport: Passed in asyncio.Transport
         """
         peername = transport.get_extra_info('peername')
-        logger.debug(f"Connection made with: {peername}")
+        logger.debug("Connection made with: {}".format(peername))
         self.transport = cast(asyncio.Transport, transport)
 
     def data_received(self, data: bytes) -> None:
@@ -47,7 +47,7 @@ class Fauxmo(asyncio.Protocol):
         """
         msg = data.decode()
 
-        logger.debug(f"Received message:\n{msg}")
+        logger.debug("Received message:\n{}".format(msg))
         if msg.startswith('GET /setup.xml HTTP/1.1'):
             logger.info("setup.xml requested by Echo")
             self.handle_setup()
@@ -65,28 +65,28 @@ class Fauxmo(asyncio.Protocol):
             '<root>'
             '<device>'
             '<deviceType>urn:Fauxmo:device:controllee:1</deviceType>'
-            f'<friendlyName>{self.name}</friendlyName>'
+            '<friendlyName>{}</friendlyName>'.format(self.name)
             '<manufacturer>Belkin International Inc.</manufacturer>'
             '<modelName>Emulated Socket</modelName>'
             '<modelNumber>3.1415</modelNumber>'
-            f'<UDN>uuid:Socket-1_0-{self.serial}</UDN>'
+            '<UDN>uuid:Socket-1_0-{}</UDN>'.format(self.serial)
             '</device>'
             '</root>'
             )
 
         setup_response = '\n'.join([
             'HTTP/1.1 200 OK',
-            f'CONTENT-LENGTH: {len(setup_xml)}',
+            'CONTENT-LENGTH: {}'.format(len(setup_xml)),
             'CONTENT-TYPE: text/xml',
-            f'DATE: {date_str}',
+            'DATE: {}'.format(date_str),
             'LAST-MODIFIED: Sat, 01 Jan 2000 00:01:15 GMT',
             'SERVER: Unspecified, UPnP/1.0, Unspecified',
             'X-User-Agent: Fauxmo',
             'CONNECTION: close\n',
-            f"{setup_xml}"
+            "{}".format(setup_xml)
             ])
 
-        logger.debug(f"Fauxmo response to setup request:\n{setup_response}")
+        logger.debug("Fauxmo response to setup request:\n{setup_response}".format(setup_response))
         self.transport.write(setup_response.encode())
         self.transport.close()
 
@@ -97,7 +97,7 @@ class Fauxmo(asyncio.Protocol):
             msg: Body of the Echo's HTTP request to trigger an action
 
         """
-        logger.debug(f"Handling action for plugin type {self.plugin}")
+        logger.debug("Handling action for plugin type {}".format(self.plugin))
 
         success = False
         soap_format = (
@@ -122,17 +122,17 @@ class Fauxmo(asyncio.Protocol):
         state_int: int = None
 
         if command_format("Get") in msg:
-            logger.info(f"Attempting to get state for {self.plugin.name}")
+            logger.info("Attempting to get state for {}".format(self.plugin.name))
 
             action = "Get"
 
             try:
                 state = self.plugin.get_state()
             except AttributeError:
-                logger.warning(f"Plugin {self.plugin.__module__} has not "
-                               "implemented a `get_state` method.")
+                logger.warning("Plugin {} has not "
+                               "implemented a `get_state` method.".format(self.plugin.__module__))
             else:
-                logger.info(f"{self.plugin.name} state: {state}")
+                logger.info("{} state: {}".format(self.plugin.name, state))
 
                 success = True
                 state_int = int(state.lower() == "on")
@@ -141,37 +141,36 @@ class Fauxmo(asyncio.Protocol):
             action = "Set"
 
             if '<BinaryState>0</BinaryState>' in msg:
-                logger.info(f"Attempting to turn off {self.plugin.name}")
+                logger.info("Attempting to turn off {}".format(self.plugin.name))
                 state_int = 0
                 success = self.plugin.off()
 
             elif '<BinaryState>1</BinaryState>' in msg:
-                logger.info(f"Attempting to turn on {self.plugin.name}")
+                logger.info("Attempting to turn on {}".format(self.plugin.name))
                 state_int = 1
                 success = self.plugin.on()
 
             else:
-                logger.warning(f"Unrecognized request:\n{msg}")
+                logger.warning("Unrecognized request:\n{}".format(msg))
 
         if success:
             date_str = formatdate(timeval=None, localtime=False, usegmt=True)
             soap_message = soap_format(action=action, state_int=state_int)
             response = '\n'.join([
                 'HTTP/1.1 200 OK',
-                f'CONTENT-LENGTH: {len(soap_message)}',
+                'CONTENT-LENGTH: {}'.format(len(soap_message)),
                 'CONTENT-TYPE: text/xml charset="utf-8"',
-                f'DATE: {date_str}',
+                'DATE: {}'.format(date_str),
                 'EXT:',
                 'SERVER: Unspecified, UPnP/1.0, Unspecified',
                 'X-User-Agent: Fauxmo',
                 'CONNECTION: close\n',
-                f'{soap_message}',
+                soap_message,
                 ])
             logger.debug(response)
             self.transport.write(response.encode())
         else:
-            errmsg = (f"Unable to complete command for {self.plugin.name}:\n"
-                      f"{msg}")
+            errmsg = "Unable to complete command for {}:\n{}".format(self.plugin.name, msg)
             logger.warning(errmsg)
         self.transport.close()
 
@@ -221,7 +220,7 @@ class SSDPServer(asyncio.DatagramProtocol):
             data: Incoming data content
             addr: Address sending data
         """
-        logger.debug(f"Received data below from {addr}:")
+        logger.debug("Received data below from {}:".format(addr))
         logger.debug(str(data))
 
         if all(b in data for b in [b'"ssdp:discover"',
@@ -241,22 +240,22 @@ class SSDPServer(asyncio.DatagramProtocol):
             ip_address = device.get('ip_address')
             port = device.get('port')
 
-            location = f'http://{ip_address}:{port}/setup.xml'
+            location = 'http://{}:{}/setup.xml'.format(ip_address, port)
             serial = make_serial(name)
             response = '\n'.join([
                 'HTTP/1.1 200 OK',
                 'CACHE-CONTROL: max-age=86400',
-                f'DATE: {date_str}',
+                'DATE: {}'.format(date_str),
                 'EXT:',
-                f'LOCATION: {location}',
+                'LOCATION: {}'.format(location),
                 'OPT: "http://schemas.upnp.org/upnp/1/0/"; ns=01',
-                f'01-NLS: {uuid.uuid4()}',
+                '01-NLS: {}'.format(uuid.uuid4()),
                 'SERVER: Fauxmo, UPnP/1.0, Unspecified',
                 'ST: urn:Belkin:device:**',
-                f'USN: uuid:Socket-1_0-{serial}::urn:Belkin:device:**',
+                'USN: uuid:Socket-1_0-{}::urn:Belkin:device:**'.format(serial),
                 ]) + '\n\n'
 
-            logger.debug(f"Sending response to {addr}:\n{response}")
+            logger.debug("Sending response to {}:\n{}".format(addr, response))
             self.transport.sendto(response.encode(), addr)
         random.shuffle(self.devices)
 
@@ -267,4 +266,4 @@ class SSDPServer(asyncio.DatagramProtocol):
             exc: Exception type
         """
         if exc:
-            logger.warning(f"SSDPServer closed with exception: {exc}")
+            logger.warning("SSDPServer closed with exception: {}".format(exc))
