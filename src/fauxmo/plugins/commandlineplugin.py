d7a7ae86d7c9dcbe8b0e5d9a823c8f78ddcadc45
Nathan Henrie <nate@n8henrie.com>
2019-09-19 09:44:00 -0600

    Migrate dep-free plugins from fauxmo-plugins

---
+++ src/fauxmo/plugins/commandlineplugin.py
@@ -0,0 +1,133 @@
+"""Fauxmo plugin that runs a command on the local machine.
+
+Runs a `shlex`ed command using `subprocess.run`, keeping the default of
+`shell=False`. This is probaby frought with security concerns, which is why
+this plugin is not included by default in `fauxmo.plugins`. By installing or
+using it, you acknowledge that it could run commands from your config.json that
+could lead to data compromise, corruption, loss, etc. Consider making your
+config.json read-only. If there are parts of this you don't understand, you
+should probably not use this plugin.
+
+If the command runs with a return code of 0, Alexa should respond prompty
+"Okay" or something that indicates it seems to have worked. If the command has
+a return code of anything other than 0, Alexa stalls for several seconds and
+subsequently reports that there was a problem (which should notify the user
+that something didn't go as planned).
+
+Note that `subprocess.run` as implemented in this plugin doesn't handle complex
+commands with pipes, redirection, or multiple statements joined by `&&`, `||`,
+`;`, etc., so you can't just use e.g. `"command that sometimes fails || true"`
+to avoid the delay and Alexa's response. If you really want to handle more
+complex commands, consider using this plugin as a template for another one
+using `os.system` instead of `subprocess.run`, but realize that this comes with
+substantial security risks that exceed my ability to explain.
+
+Example config:
+```
+{
+  "FAUXMO": {
+    "ip_address": "auto"
+  },
+  "PLUGINS": {
+    "CommandLinePlugin": {
+      "path": "/path/to/commandlineplugin.py",
+      "DEVICES": [
+        {
+            "name": "output stuff to a file",
+            "port": 49915,
+            "on_cmd": "touch testfile.txt",
+            "off_cmd": "rm testfile.txt",
+            "state_cmd": "ls testfile.txt",
+        }
+      ]
+    }
+  }
+}
+```
+"""
+
+import shlex
+import subprocess
+
+from fauxmo.plugins import FauxmoPlugin
+
+
+class CommandLinePlugin(FauxmoPlugin):
+    """Fauxmo Plugin for running commands on the local machine."""
+
+    def __init__(
+        self,
+        name: str,
+        port: int,
+        on_cmd: str,
+        off_cmd: str,
+        state_cmd: str = None,
+    ) -> None:
+        """Initialize a CommandLinePlugin instance.
+
+        Args:
+            name: Name for this Fauxmo device
+            port: Port on which to run a specific CommandLinePlugin instance
+            on_cmd: Command to be called when turning device on
+            off_cmd: Command to be called when turning device off
+            state_cmd: Command to check device state (return code 0 == on)
+
+        """
+        self.on_cmd = on_cmd
+        self.off_cmd = off_cmd
+        self.state_cmd = state_cmd
+
+        super().__init__(name=name, port=port)
+
+    def run_cmd(self, cmd: str) -> bool:
+        """Partialmethod to run command.
+
+        Args:
+            cmd: Command to be run
+        Returns:
+            True if command seems to have run without error
+
+        """
+        shlexed_cmd = shlex.split(cmd)
+        process = subprocess.run(shlexed_cmd)
+        return process.returncode == 0
+
+    def on(self) -> bool:
+        """Run on command.
+
+        Returns:
+            True if command seems to have run without error.
+
+        """
+        return self.run_cmd(self.on_cmd)
+
+    def off(self) -> bool:
+        """Run off command.
+
+        Returns:
+            True if command seems to have run without error.
+
+        """
+        return self.run_cmd(self.off_cmd)
+
+    def get_state(self) -> str:
+        """Get device state.
+
+        NB: Return code of `0` (i.e. ran without error) indicates "on" state,
+        otherwise will be off. making it easier to have something like `ls
+        path/to/pidfile` suggest `on`. Many command line switches may not
+        actually have a "state" per se (just an arbitary command you want to
+        run), in which case you could just put "false" as the command, which
+        should always return "off".
+
+        Returns:
+            "on" or "off" if `state_cmd` is defined, "unknown" if undefined
+
+        """
+        if self.state_cmd is None:
+            return "unknown"
+
+        returned_zero = self.run_cmd(self.state_cmd)
+        if returned_zero is True:
+            return "on"
+        return "off"
---
+++ src/fauxmo/plugins/homeassistantplugin.py
@@ -0,0 +1,159 @@
+"""Fauxmo plugin to interact with Home Assistant devices.
+
+One simple way to find your entity_id is to use curl and pipe to grep or jq:
+
+    curl -s http://IP:PORT/api/states | jq
+
+NB: This is just a special case of the RESTAPIPlugin (or even SimpleHTTPPlugin,
+see `config-sample.json` in the main Fauxmo repo), but it makes config
+substantially easier by not having to redundantly specify headers and
+endpoints.
+
+Install to Fauxmo by downloading or cloning and including in your Fauxmo
+config. One easy way to make a long-lived access token is by using the frontend
+and going to the `/profile` endpoint, scroll to the bottom. Documentation on
+the long-lived tokens is available at
+https://developers.home-assistant.io/docs/en/auth_api.html#long-lived-access-token
+
+Example config:
+```
+{
+    "FAUXMO": {
+        "ip_address": "auto"
+    },
+    "PLUGINS": {
+        "HomeAssistantPlugin": {
+            "ha_host": "192.168.0.50",
+            "ha_port": 8123,
+            "ha_protocol": "http",
+            "ha_token": "abc123",
+            "path": "/path/to/homeassistantplugin.py",
+            "DEVICES": [
+                {
+                    "name": "example Home Assistant device 1",
+                    "port": 12345,
+                    "entity_id": "switch.my_fake_switch"
+                },
+                {
+                    "name": "example Home Assistant device 2",
+                    "port": 12346,
+                    "entity_id": "cover.my_fake_cover"
+                }
+            ]
+        }
+    }
+}
+```
+"""
+
+import json
+import urllib.parse
+import urllib.request
+from http.client import HTTPResponse
+
+from fauxmo.plugins import FauxmoPlugin
+
+
+class HomeAssistantPlugin(FauxmoPlugin):
+    """Fauxmo plugin for HomeAssistant REST API.
+
+    Allows users to specify Home Assistant services in their config.json and
+    toggle these with the Echo.
+    """
+
+    service_map = {
+        "cover": {
+            "on": "open_cover",
+            "off": "close_cover",
+            "on_state": "open",
+            "off_state": "closed",
+        },
+        "homeassistant": {"on": "turn_on", "off": "turn_off"},
+        "light": {"on": "turn_on", "off": "turn_off"},
+        "media_player": {"on": "turn_on", "off": "turn_off"},
+        "switch": {"on": "turn_on", "off": "turn_off"},
+    }
+
+    def __init__(
+        self,
+        name: str,
+        port: int,
+        entity_id: str,
+        ha_host: str,
+        ha_port: int = 8123,
+        ha_protocol: str = "http",
+        ha_token: str = None,
+    ) -> None:
+        """Initialize a HomeAssistantPlugin instance.
+
+        Args:
+            ha_token: Long-lived HomeAssistant token
+            entity_id: `entity_id` used by HomeAssistant
+            ha_host: Host running HomeAssistant
+            ha_port: Port number for HomeAssistant access
+            ha_protocol: http or https
+
+        """
+        self.ha_url = f"{ha_protocol}://{ha_host}:{ha_port}"
+
+        self.entity_id = entity_id
+
+        self.domain = self.entity_id.split(".")[0]
+        if self.domain == "group":
+            self.domain = "homeassistant"
+
+        self.headers = {
+            "Authorization": f"Bearer {ha_token}",
+            "content-type": "application/json",
+        }
+        super().__init__(name=name, port=port)
+
+    def send(self, signal: str) -> bool:
+        """Send `signal` as determined by service_map.
+
+        Args:
+            signal: the signal the service should recongize
+
+        """
+        url = f"{self.ha_url}/api/services/{self.domain}/{signal}"
+        data = {"entity_id": self.entity_id}
+
+        req = urllib.request.Request(
+            url=url,
+            headers=self.headers,
+            data=json.dumps(data).encode("utf8"),
+            method="POST",
+        )
+        with urllib.request.urlopen(req) as r:
+            if isinstance(r, HTTPResponse):
+                return r.status == 200
+            else:
+                return False
+
+    def on(self) -> bool:
+        """Turn the Home Assistant device on.
+
+        Returns: Whether the device seems to have been turned on.
+        """
+        on_cmd = HomeAssistantPlugin.service_map[self.domain.lower()]["on"]
+        return self.send(on_cmd)
+
+    def off(self) -> bool:
+        """Turn the Home Assistant device off.
+
+        Returns: Whether the device seems to have been turned off.
+        """
+        off_cmd = HomeAssistantPlugin.service_map[self.domain.lower()]["off"]
+        return self.send(off_cmd)
+
+    def get_state(self) -> str:
+        """Query the state of the Home Assistant device.
+
+        Returns: Device state as reported by HomeAssistant
+        """
+        url = f"{self.ha_url}/api/states/{self.entity_id}"
+
+        req = urllib.request.Request(url=url, headers=self.headers)
+        with urllib.request.urlopen(req) as r:
+            response = r.read().decode("utf")
+        return json.loads(response)["state"]
