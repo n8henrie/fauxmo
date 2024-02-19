351ba18335cff454a2ddb8a94b58a77fdceae155
Nathan Henrie <nate@n8henrie.com>
2024-02-19 11:02:19 -0700

    format

--- flake.nix
+++ flake.nix
@@ -30,12 +30,7 @@
             uvloop
             (buildPythonPackage {
               inherit pname;
-              version =
-                builtins.head
-                (lib.findFirst (v: v != null)
-                  null (builtins.map
-                    (builtins.match "^__version__ = \"(.*)\"")
-                    (lib.splitString "\n" (builtins.readFile ./src/fauxmo/__init__.py))));
+              version = builtins.head (lib.findFirst (v: v != null) null (builtins.map (builtins.match "^__version__ = \"(.*)\"") (lib.splitString "\n" (builtins.readFile ./src/fauxmo/__init__.py))));
               src = ./.;
               format = "pyproject";
               propagatedBuildInputs = [setuptools];
