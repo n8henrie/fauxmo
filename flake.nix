177d2e977da70e09a932e93b85464e17e466646b
Nathan Henrie <nate@n8henrie.com>
2024-02-19 09:02:26 -0700

    Update inputs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Flake for github.com/n8henrie/fauxmo";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
 
   outputs = {
     self,
