fc0210d38d8d73ce104dc8a718959ed324083451
Nathan Henrie <nate@n8henrie.com>
2025-11-01 20:40:29 -0600

    Create config in nix
    Also changes to use DynamicUser and real secrets

--- module.nix
+++ module.nix
@@ -7,19 +7,40 @@
 let
   service_name = "fauxmo";
   cfg = config.services.${service_name};
+  settingsFormat = pkgs.formats.json { };
 in
-with lib;
 {
-  options.services.${service_name} = {
+  options.services.${service_name} = with lib; {
     enable = mkEnableOption service_name;
     user = mkOption {
       type = types.str;
       description = "User that fauxmo will run as";
       default = "fauxmo";
     };
-    configFile = mkOption {
+    ip_address = mkOption {
       type = types.str;
-      description = "Path to config.json";
+      description = "IP addresses that will be advertised to the Alexa";
+      default = "auto";
+    };
+    plugins = mkOption {
+      inherit (settingsFormat) type;
+      description = ''Plugins and plugin settings for fauxmo. Example: <https://github.com/n8henrie/fauxmo/blob/master/config-sample.json>'';
+      default = { };
+    };
+    secretsFile = mkOption {
+      type = lib.types.path;
+      description = ''
+        JSON file containing secret keys that will be merged into the config. A dummy example:
+        ```
+        {
+            "PLUGINS": {
+                HomeAssistantPlugin: {
+                    "ha_token": "abc123..."
+                }
+            }
+        }
+        ```
+      '';
     };
     verbosity = mkOption {
       type = types.enum [
@@ -36,72 +57,65 @@ with lib;
       default = false;
       description = lib.mdDoc "Whether to open TCP ports in the firewall";
     };
-    tcpPortRange = mkOption {
-      description = "TCP range to open for fauxmo";
-      type =
-        with types;
-        submodule {
-          options = {
-            from = mkOption {
-              type = int;
-            };
-            to = mkOption {
-              type = int;
-            };
-          };
-        };
-    };
   };
 
-  config = mkIf cfg.enable {
-    networking.firewall = mkIf cfg.openFirewall {
-      allowedUDPPorts = [
-        1900
-      ];
-      allowedTCPPortRanges = [
-        {
-          inherit (cfg.tcpPortRange) to from;
-        }
-      ];
-    };
-    systemd.services.${service_name} =
-      let
-        pythonWithFauxmo = pkgs.python3.withPackages (
-          ps: with ps; [
-            fauxmo
-            uvloop
-          ]
+  config =
+    let
+      inherit (lib) mkIf;
+    in
+    mkIf cfg.enable {
+      networking.firewall = mkIf cfg.openFirewall {
+        allowedUDPPorts = [
+          1900
+        ];
+        allowedTCPPorts = builtins.concatMap (plugin: builtins.map (d: d.port) plugin.DEVICES) (
+          builtins.attrValues cfg.plugins
         );
-        after = [ "network-online.target" ];
-      in
-      {
-        description = service_name;
-        inherit after;
-        requires = after;
-        script =
-          let
-            verbosity =
-              if cfg.verbosity == 0 then
-                ""
-              else
-                "-" + lib.concatStrings (builtins.genList (_: "v") cfg.verbosity);
-          in
-          ''${pythonWithFauxmo}/bin/python -m fauxmo.cli ${verbosity} -c "${cfg.configFile}"'';
-        serviceConfig = {
-          User = cfg.user;
-          Restart = "on-failure";
-          RestartSec = 30;
-        };
-        wantedBy = [ "multi-user.target" ];
       };
-    users = mkIf (cfg.user == "fauxmo") {
-      users = {
-        fauxmo = {
-          isSystemUser = true;
-          group = "fauxmo";
+      systemd.services.${service_name} =
+        let
+          after = [ "network-online.target" ];
+        in
+        {
+          description = service_name;
+          inherit after;
+          requires = after;
+          script =
+            let
+              pythonWithFauxmo = lib.getExe (
+                pkgs.python3.withPackages (
+                  ps: with ps; [
+                    fauxmo
+                    uvloop
+                  ]
+                )
+              );
+              verbosity =
+                if cfg.verbosity == 0 then
+                  ""
+                else
+                  "-" + lib.concatStrings (builtins.genList (_: "v") cfg.verbosity);
+            in
+            "${pythonWithFauxmo} -m fauxmo.cli ${verbosity} -c /run/fauxmo/config.json";
+          serviceConfig = {
+            ExecStartPre =
+              let
+                configFile = settingsFormat.generate "config.json" {
+                  FAUXMO.ip_address = cfg.ip_address;
+                  PLUGINS = cfg.plugins;
+                };
+              in
+              ''
+                /bin/sh -c "${pkgs.jq}/bin/jq -s '.[0] * .[1]' ${configFile} %d/secrets.json > /run/fauxmo/config.json"
+              '';
+            LoadCredential = "secrets.json:${cfg.secretsFile}";
+            User = "fauxmo";
+            RuntimeDirectory = "fauxmo";
+            DynamicUser = true;
+            Restart = "on-failure";
+            RestartSec = 30;
+          };
+          wantedBy = [ "multi-user.target" ];
         };
-      };
-      groups.fauxmo = { };
     };
-  };
 }
